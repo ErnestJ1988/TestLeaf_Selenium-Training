c09767ebc6fbef114f4d2975579a00ca417bc456
ErnestJ1988 <168218966+ErnestJ1988@users.noreply.github.com>
2024-04-27 17:56:16 +0530

    Add files via upload

---
+++ DataTypes1.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class DataTypes1 {
+
+	
+	public static void main(String[] args) {
+		
+		int tabopened = 2;
+		float version = 22.31f;
+		char firstLetter = 'C';
+		boolean loginsuccessfull = true;
+		String Browser = "Chrome";
+		
+		System.out.println("Tab Opened is" + tabopened);
+		System.out.println("Browser version is "+ Browser + " its version is" + version );
+		
+	}
+}
